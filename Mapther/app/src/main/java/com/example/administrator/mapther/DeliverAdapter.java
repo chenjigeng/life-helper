724acf83677ed5818fc50bcbc4172626d57a2c87
陈纪庚 <178854407@qq.com>
2016-12-14 16:22:54 +0800

    first commit

---
+++ Mapther/app/src/main/java/com/example/administrator/mapther/DeliverAdapter.java
@@ -0,0 +1,8 @@
+package com.example.administrator.mapther;
+
+/**
+ * Created by chenjg on 2016/12/8.
+ */
+
+public class DeliverAdapter {
+}
---
+++ Mapther/app/src/main/java/com/example/administrator/mapther/UserActivity.java
@@ -0,0 +1,8 @@
+package com.example.administrator.mapther;
+
+/**
+ * Created by chenjg on 2016/12/9.
+ */
+
+public class UserActivity {
+}
